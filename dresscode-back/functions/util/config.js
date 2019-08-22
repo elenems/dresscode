0cdeb19e2a27713a7db0564732953b2152b241a4
elenems <mickhaelyarish@gmail.com>
2019-08-22 15:12:52 +0300

    Update config.js
    Removed firebase config

--- dresscode-back/functions/util/config.js
+++ dresscode-back/functions/util/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  apiKey: "AIzaSyCraWQvVlpNJxMA0SP-kM5Req6yUqLM0Zw",
-  authDomain: "dresscode-691e5.firebaseapp.com",
-  databaseURL: "https://dresscode-691e5.firebaseio.com",
-  projectId: "dresscode-691e5",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
   storageBucket: "",
-  messagingSenderId: "393517814902",
-  appId: "1:393517814902:web:413503eb4332d393"
+  messagingSenderId: "",
+  appId: ""
   };
